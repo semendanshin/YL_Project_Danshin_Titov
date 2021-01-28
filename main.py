a60a0a5f60fcc50c38f203d9796b503c75e11266
Titov <Titovfedor22@yandex.ru>
2021-01-28 12:41:00 +0300

    Добавлены стандартные функции, шапка, запуск. Начал разработку меню. Заложил основу главным методам

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
+import pygame, os
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
 class Menu:
-    def __init__(self):
+    def __init__(self, screen):
+        self.screen = screen
+        self.running = True
+
+    def main_loop(self):
+        while self.running:
+            for event in pygame.event.get():
+                pass
+            self.update()
+            self.render()
+            pygame.display.update()
+
+    def update(self):
         pass
 
+    def render(self):
+        pass
 
 class Settings:
     def __init__(self):
         pass
 
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('раннер')
+    display_size = pygame.display.Info().current_w, pygame.display.Info().current_h
+    SIZE = WIDTH, HEIGHT = 1920, 1080
+    screen = pygame.display.set_mode(SIZE)
+    menu = Menu(screen)
