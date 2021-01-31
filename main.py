e94d4823478d71dc3c8ebe68047f277fffb47e81
Titov <Titovfedor22@yandex.ru>
2021-02-01 00:22:15 +0300

    Написан класс меню и создан класс CheckBox. В след версии надо резализовать обновление файла с настройками, более красивое расположение кнопок, возможность узнать какая именно кнопка нажалась, и внести данные о самих настройках

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ class MySpriteGroup(pygame.sprite.Group):
         for el in self.sprites():
             el.draw(screen)
 
+    def update(self, ind):
+        for el in self.sprites():
+            el.update(ind)
+
 
 class ClickButton(pygame.sprite.Sprite):
     def __init__(self, name, func, i, X, Y, *groups):
@@ -41,6 +45,37 @@ class ClickButton(pygame.sprite.Sprite):
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
+    def update(self, *args):
+        pass
+
+
+class CheckBox(pygame.sprite.Sprite):
+    def __init__(self, i, X, Y, *groups):
+        super().__init__(*groups)
+        image0 = load_im('check0.png').convert_alpha()
+        image1 = load_im('check1.png').convert_alpha()
+        k = image1.get_height() / image1.get_width()
+        self.image0 = pygame.transform.scale(image0, (X // 10, int(X // 10 * k)))
+        self.image1 = pygame.transform.scale(image1, (X // 10, int(X // 10 * k)))
+        self.i = i
+        self.image = self.image0
+        self.rect = self.image.get_rect()
+        x, y = X // 2, Y // 2
+        x += (self.rect.width + 20) * i
+        self.rect.x, self.rect.y = x - self.rect.width // 2, y - self.rect.height // 2
+        self.update(3)
+
+    def update(self, ind):
+        if ind == 4:
+            return None
+        if self.i == ind:
+            self.image = self.image1
+        else:
+            self.image = self.image0
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
 
 class Menu:
     def __init__(self, screen):
@@ -48,7 +83,7 @@ class Menu:
         self.running = True
         self.buttons = MySpriteGroup()
         ClickButton('button1.png', pp1, 0, screen.get_width(), screen.get_height(), self.buttons)
-        ClickButton('button2.png', pp2, 1, screen.get_width(), screen.get_height(), self.buttons)
+        ClickButton('button2.png', settings.main_loop, 1, screen.get_width(), screen.get_height(), self.buttons)
         ClickButton('button3.png', pp3, 2, screen.get_width(), screen.get_height(), self.buttons)
 
     def main_loop(self):
@@ -76,8 +111,35 @@ class Menu:
 
 
 class Settings:
-    def __init__(self):
-        pass
+    def __init__(self, screen):
+        self.screen = screen
+        self.running = True
+        self.checkboxs = MySpriteGroup()
+        for _ in range(4):
+            CheckBox(_, screen.get_width(), screen.get_height(), self.checkboxs)
+
+    def main_loop(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE or event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    self.update(event.pos)
+                self.render()
+            pygame.display.update()
+        self.running = True
+
+    def update(self, pos):
+        i = 0
+        for sprite in self.checkboxs.sprites():
+            if sprite.rect.collidepoint(pos):
+                break
+            i += 1
+        self.checkboxs.update(i)
+
+    def render(self):
+        self.screen.fill((100, 0, 0))
+        self.checkboxs.draw(self.screen)
 
 
 if __name__ == '__main__':
@@ -86,5 +148,6 @@ if __name__ == '__main__':
     display_size = pygame.display.Info().current_w, pygame.display.Info().current_h
     SIZE = WIDTH, HEIGHT = 1920, 1080
     screen = pygame.display.set_mode(SIZE)
+    settings = Settings(screen)
     menu = Menu(screen)
     menu.main_loop()
